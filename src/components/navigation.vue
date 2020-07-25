7d7ee8b8ec4b93967508de188b9f38de81050a97
Ahmed-Mahmoud <ahmedm.monte@gmail.com>
2020-07-26 01:42:17 +0200

    Simple_NavBar

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -1,6 +1,6 @@
 <template>
+    <nav>
     <ul>
-
         <li>
             <router-link
                 :to="{name:'Home'}"
@@ -10,7 +10,8 @@
         </li>
         <template v-if="authenticated">
             <li>
-                {{ user.name }}
+                <a href=""> {{ user.name }}</a>
+
             </li>
         <li>
             <router-link
@@ -40,6 +41,7 @@
             </li>
         </template>
     </ul>
+    </nav>
 </template>
 
 <script>
@@ -63,3 +65,34 @@
         }
     }
 </script>
+<style lang="scss">
+    nav{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        min-height: 8vh;
+        background-color: black;
+        margin-block-start: 0;
+        margin-block-end: 0;
+        padding-inline-start: 0;
+        margin-top: 0;
+
+        ul{
+
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        li {
+            float: left;
+        }
+
+        a {
+            display: block;
+            padding: 8px;
+            color: white;
+
+        }
+    }
+</style>
