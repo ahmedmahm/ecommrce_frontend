c5b746fe9848ca5d64fa16923ab9719e307284db
Ahmed-Mahmoud <ahmedm.monte@gmail.com>
2020-07-27 01:25:56 +0200

    fix_login_conflict

--- src/views/signin.vue
+++ src/views/signin.vue
@@ -58,33 +58,6 @@
       </b-col>
     </b-row>
   </b-container>
-=======
-  <form @submit.prevent="submit">
-    <div>
-      <label for="email">Email</label>
-      <input type="text" name="email" id="email" v-model="form.email" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        v-model="form.password"
-      />
-    </div>
-
-    <div>
-      <button type="submit">
-        Sign in
-      </button>
-    </div>
-    <div>
-      <label>New Customer? </label>
-      <router-link :to="{ name: 'register' }">Register</router-link>
-    </div>
-  </form>
-
 </template>
 
 <script>
