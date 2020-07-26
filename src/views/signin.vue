01a30cd2f80537cbd0ca799882b8da7551bfdea1
Abudi <mohamed@heartbeat-med.de>
2020-07-26 18:36:03 +0200

    hey

--- src/views/signin.vue
+++ src/views/signin.vue
@@ -1,54 +1,57 @@
 <template>
-    <form @submit.prevent="submit">
-        <div>
-            <label for="email">Email</label>
-            <input type="text" name="email" id="email" v-model="form.email">
-        </div>
-       <div>
-           <label for="password">Password</label>
-           <input type="password" name="password" id="password" v-model="form.password">
-       </div>
+  <form @submit.prevent="submit">
+    <div>
+      <label for="email">Email</label>
+      <input type="text" name="email" id="email" v-model="form.email" />
+    </div>
+    <div>
+      <label for="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        v-model="form.password"
+      />
+    </div>
 
-        <div>
-            <button type="submit">
-                Sign in
-            </button>
-        </div>
-        <div>
-            <label>New Customer?   </label>
-            <router-link
-            :to="{name:'register'}">Register</router-link>
-        </div>
-    </form>
-
+    <div>
+      <button type="submit">
+        Sign in
+      </button>
+    </div>
+    <div>
+      <label>New Customer? </label>
+      <router-link :to="{ name: 'register' }">Register</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-    import { mapActions } from 'vuex'
-    export default {
-        name: 'signin',
-        components: {
-            //
-        },
-        data () {
-            return {
-                form: {
-                    email: '',
-                    password: '',
-                }
-            }
-        },
-        methods:{
-            ...mapActions({
-                signin : 'auth/signIn'
-            }),
-           submit () {
-                this.signin(this.form).then(() => {
-                    this.$router.replace({
-                        name: 'dashboard'
-                    })
-                })
-            }
-        }
-    }
+import { mapActions } from "vuex";
+export default {
+  name: "signin",
+  components: {
+    //
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    ...mapActions({
+      signin: "auth/signIn",
+    }),
+    submit() {
+      this.signin(this.form).then(() => {
+        this.$router.replace({
+          name: "dashboard",
+        });
+      });
+    },
+  },
+};
 </script>
